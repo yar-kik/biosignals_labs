f7c4f7f8ed7c06934e567e76dff3fa7fd09a3bbc
yar-kik <einstein16.04@gmail.com>
2020-11-22 14:21:24 +0200

    updated task 4

--- laba4/laba4.4.py
+++ laba4/laba4.4.py
@@ -13,27 +13,32 @@ sample_rate = 512
 duration = 3
 frequency1 = 10
 frequency2 = 100
-f = [frequency1, frequency2]
-time = np.linspace(0, duration, duration * sample_rate)
-frequency = np.linspace(0, sample_rate, duration * sample_rate)
-x1 = square(2 * pi * frequency1 * time)
-x2 = square(2 * pi * frequency2 * time)
-x12 = [x1, x2]
+t = np.linspace(0, duration, duration * sample_rate)
+f = np.linspace(0, sample_rate, duration * sample_rate)
+x1 = square(2 * pi * frequency1 * t)
+x2 = square(2 * pi * frequency2 * t)
 
 y1 = 2 * np.abs(fft(x1) / len(x1))
 y2 = 2 * np.abs(fft(x2) / len(x2))
+
+frequency = [frequency1, frequency2]
+x12 = [x1, x2]
 y12 = [y1, y2]
 
-
-fig, axes = plt.subplots(2, 2, constrained_layout=True)
-fig.set_size_inches(8, 6)
+x_label = ['Час, с', 'Частота, Гц', 'Частота, Гц']
+title = [['Сигнал прямокутних імпульсів частоти 10 Гц', 'Сигнал прямокутних імпульсів частоти 100 Гц'],
+         ['Спектр сигналу', 'Спектр сигналу'],
+         ['Спектр сигналу на нижніх частотах', 'Спектр сигналу на нижніх частотах']]
+fig, axes = plt.subplots(2, 3, constrained_layout=True)
+fig.set_size_inches(12, 6)
 for i, ax in enumerate(axes):
-    ax[0].plot(time, x12[i], linewidth=.7)
-    ax[1].stem(frequency, y12[i])
+    ax[0].plot(t, x12[i])
+    ax[1].stem(f, y12[i])
     ax[1].set_xlim(0, sample_rate / 2)
-    ax[0].set_xlabel('Час, с')
-    ax[1].set_xlabel('Частота, Гц')
-    # ax.set_ylabel("Амплітуда, В")
-    ax[0].set_title(f"Частота {f[i]} Гц")
-    ax[1].set_title("Спектр")
+    ax[2].stem(f, y12[i])
+    ax[2].set_xlim(0, sample_rate / 32)
+    for j in range(3):
+        ax[j].set_xlabel(x_label[j])
+        ax[j].set_title(title[j][i])
+        ax[j].set_ylabel("Амплітуда")
 plt.show()
