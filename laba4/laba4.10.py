3809726855d8f02aad6c62fc774274728e758cfc
yar-kik <einstein16.04@gmail.com>
2020-11-24 00:09:12 +0200

    updated task 10

--- laba4/laba4.10.py
+++ laba4/laba4.10.py
@@ -1,36 +1,40 @@
 """10. Побудувати функцію для розрахунку та виводу на графік спектральної густини потужності сигналу, прочитаного з
 файлу. В функцію передавати назву файлу з сигналом та інші необхідні дані. """
+from typing import Union
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft
 from scipy.io import loadmat
 
-ekg_normal = np.load('../data/norm/1600715877.npz')
-ekg_anomaly = np.load('../data/anomaly/1600717447.npz')
-sample_rate = ekg_normal['fs']
-signal = ekg_normal['signal']
-signal = ekg_anomaly['signal']
+
+def build_spectral_density(x: np.array, fs: Union[int, float]) -> None:
+    duration = len(x) / fs
+    t = np.linspace(0, duration, len(x))
+    f = np.linspace(0, fs, len(x))
+    y = 2 * np.abs(fft(x) / len(x))
+    power_spectral_density = y ** 2
+
+    x_label = ['Час, с', 'Частота, Гц']
+    y_label = ["Амплітуда, мкВ", r"Спектральна густина, $мкВ^2$/Гц"]
+    title = ['Сигнал', 'Спектральна густина потужності сигналу']
+
+    fig, ax = plt.subplots(2, constrained_layout=True)
+    fig.set_size_inches(12, 6)
+    ax[0].plot(t, x)
+    ax[1].plot(f, power_spectral_density)
+    ax[1].set_xlim(0, fs / 16)
+    for j in range(2):
+        ax[j].set_xlabel(x_label[j])
+        ax[j].set_title(title[j])
+        ax[j].set_ylabel(y_label[j])
+        ax[j].minorticks_on()
+        ax[j].grid(which='major', linewidth=1.2)
+        ax[j].grid(which='minor', linewidth=.5)
+    plt.show()
+
 
 eeg_healthy = loadmat('../data/EEG_healthy/eeg_healthy_9.mat')
 signal = eeg_healthy['sig'][0]
 sample_rate = 256
-
-duration = len(signal) / sample_rate
-
-time = np.linspace(0, duration, len(signal))
-frequency = np.linspace(0, sample_rate, len(signal))
-y = 2 * np.abs(fft(signal) / len(signal))
-power_spectral_density = y ** 2
-
-fig, axes = plt.subplots(2, constrained_layout=True)
-fig.set_size_inches(8, 6)
-axes[0].plot(time, signal)
-axes[1].plot(frequency, power_spectral_density)
-axes[1].set_xlim(0, sample_rate / 2)
-axes[0].set_xlabel('Час, с')
-axes[1].set_xlabel('Частота, Гц')
-# ax.set_ylabel("Амплітуда, В")
-# ax[0].set_title(f"Частота {f[i]} Гц")
-axes[1].set_title("Спектр")
-plt.show()
+build_spectral_density(signal, sample_rate)
