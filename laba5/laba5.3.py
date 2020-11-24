632b1935ac629756e02c93b4f178285e12d0845c
yar-kik <einstein16.04@gmail.com>
2020-11-24 16:51:36 +0200

    updated task 3

--- laba5/laba5.3.py
+++ laba5/laba5.3.py
@@ -11,11 +11,13 @@ from typing import Union
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal.windows import flattop
+from scipy.signal import spectrogram
 
 
 def single_square(t: np.array, tau: Union[int, float]):
-    """"""
+    """
+    Функція для свторення прямокутного імпульсу
+    """
     x = np.zeros(len(t))
     for k, tk in enumerate(t):
         if np.abs(tk) > tau/2:
@@ -33,9 +35,7 @@ frequency = 40
 pulse_duration = 1
 time_shift = 10
 window_duration = .2
-NFFT = int(window_duration * sample_rate)
-
-window = flattop(NFFT)
+nperseg = int(window_duration * sample_rate)
 
 x_sin = np.sin(2 * np.pi * frequency * time)
 x_square = single_square(time - time_shift, pulse_duration)
@@ -43,10 +43,21 @@ x_random = np.random.random(len(time))
 x_mixed = x_sin + x_square + x_random
 
 x = [x_sin, x_square, x_random, x_mixed]
-for j in range(len(x)):
-    fig, axes = plt.subplots(2, constrained_layout=True)
-
-    # for i, ax in enumerate(axes):
-    axes[0].plot(time, x[j])
-    axes[1].specgram(x[j], Fs=sample_rate, NFFT=NFFT, noverlap=0, window=window)
-    plt.show()
+x_label = ["Час, с", "Час, с"]
+y_label = ["Амплітуда", "Частота, Гц"]
+title = [["Синусоїда частоти 40 Гц", "Спектрограма"],
+         ["Прямокутний імпульс ширини 1 с", "Спектрограма"],
+         ["Випадковий сигнал", "Спектрограма"],
+         ["Сума сигналів", "Спектрограма"]]
+figure, axes = plt.subplots(len(x), 2, constrained_layout=True)
+figure.set_size_inches(12, 6)
+for i, ax in enumerate(axes):
+    f, t, y = spectrogram(x[i], fs=sample_rate, window='flattop', noverlap=0, nperseg=nperseg)
+    ax[0].plot(time, x[i])
+    pcm = ax[1].pcolormesh(t, f, y, shading='gouraud')
+    figure.colorbar(pcm, ax=ax[1])
+    for j in range(2):
+        ax[j].set_xlabel(x_label[j])
+        ax[j].set_title(title[i][j])
+        ax[j].set_ylabel(y_label[j])
+plt.show()
