3bd1d496f80fa9dc000d6c5fcb17e6bb1211bb6a
yar-kik <einstein16.04@gmail.com>
2020-11-24 15:44:25 +0200

    updated task 2

--- laba5/laba5.2.py
+++ laba5/laba5.2.py
@@ -11,21 +11,32 @@ from scipy.signal.windows import flattop
 
 duration = 1
 sample_rate = 128
-time = np.linspace(0, duration, duration * sample_rate, endpoint=False)
-freq = np.linspace(0, sample_rate, duration * sample_rate)
+time = np.arange(0, duration, 1 / sample_rate)
+freq = np.arange(0, sample_rate, 1 / duration)
 
 frequency = [2, 2.5]
-
-fig, axes = plt.subplots(2, 3, constrained_layout=True)
+title = [["Сигнал з частотою 2 Гц", "Сигнал з частотою 2.5 Гц"],
+         ['Амплітудний спектр без віконної функції', 'Амплітудний спектр без віконної функції'],
+         ['Амплітудний спектр із віконною функцією', 'Амплітудний спектр із віконною функцією']]
+x_label = ["Час, с", "Частота, Гц", "Частота, Гц"]
+figure, axes = plt.subplots(2, 3, constrained_layout=True)
+figure.set_size_inches(12, 6)
 for i, ax in enumerate(axes):
     x = np.sin(2 * np.pi * frequency[i] * time)
     y = 2 * np.abs(fft(x) / len(x))
-    z = 2 * np.abs(fft(x * flattop(len(x))) / len(x))
+    y_window = 2 * np.abs(fft(x * flattop(len(x))) / len(x))
     ax[0].plot(time, x)
     ax[1].stem(freq, y)
-    ax[2].stem(freq, z)
-    ax[2].set_xlim(0, sample_rate // 2)
-    ax[1].set_xlim(0, sample_rate // 2)
+    ax[1].set_xlim(0, sample_rate / 2)
+    ax[2].stem(freq, y_window)
+    ax[2].set_xlim(0, sample_rate / 2)
+    for j in range(3):
+        ax[j].set_xlabel(x_label[j])
+        ax[j].set_title(title[j][i])
+        ax[j].set_ylabel("Амплітуда")
+        ax[j].minorticks_on()
+        ax[j].grid(which='major', linewidth=1.2)
+        ax[j].grid(which='minor', linewidth=.5)
 plt.show()
 
 
