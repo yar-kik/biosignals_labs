a632c7ecd9323b4fd6ae15a41865322e7bd49b2b
yar-kik <einstein16.04@gmail.com>
2020-12-15 20:02:06 +0200

    completed task 1

--- laba7/laba7.1.py
+++ laba7/laba7.1.py
@@ -9,14 +9,14 @@ import matplotlib.pyplot as plt
 duration = 1
 sample_rate = 256
 t = np.linspace(0, duration, duration * sample_rate)
-x = np.random.randn(sample_rate * duration)
-y = np.correlate(x, x, mode="full")
+x = np.random.randn(len(t))
+y = np.correlate(x, x, mode="full") / len(x)  # нормування значень
 
 plt.figure(num=1, figsize=(8, 6))
 plt.stem(y)
-plt.title("", fontsize=14)
-plt.xlabel("", fontsize=10)
-plt.ylabel("", fontsize=10)
+plt.title("Графік автокореляційної функції", fontsize=14)
+plt.xlabel("Лаг", fontsize=10)  # лаг - величина зсуву в часі
+plt.ylabel("Автокореляція", fontsize=10)
 plt.minorticks_on()
 plt.grid(which='major', linewidth=1.2)
 plt.grid(which='minor', linewidth=.5)
